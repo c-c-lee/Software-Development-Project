6f65e09ad4d141e7c7ee45f67a31d63f279bafe6
Siril Sunil <sirilsunil17@gmail.com>
2024-02-19 13:11:50 +0000

    Updated app.py

---
+++ Flask_Project/app.py
@@ -0,0 +1,25 @@
+from flask import Flask                 #also import render template, request..
+from flask_sqlalchemy import SQLAlchemy
+#import sql queries and other funtions from relevant directories
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ArchGenome.db'    # Ensure correct path to database is here
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+# Define routes for backend here: 
+
+#@app.route("/")
+#def home():
+    #return render_template("index.html")
+
+
+#if __name__ == '__main__':
+    #app.run(debug=True)
+
+
+
+
+
