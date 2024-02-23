590f75e70bf6b0e6db0895e26fa3554d761e4d26
Your Name <cheaulee@gmail.com>
2024-02-23 12:10:44 +0000

    Updated SQL schema (path issues)

--- Flask_Project/src/database/db_schema.py
+++ Flask_Project/src/database/db_schema.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Flask_Project/src/database/instance/ArchGenome.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ArchGenome.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
