1559d713907b73af5023960c115c9493c6960114
farzhmz <112782889+farzhmz@users.noreply.github.com>
2024-02-24 00:10:32 +0000

    Fixed header row in database and updated data

--- Flask_Project/src/database/db_schema.py
+++ Flask_Project/src/database/db_schema.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ArchGenome.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://///Users/farzadhamzawe/group_project bioinformatics/Software-Development-Project/instance/ArchGenome.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -71,3 +71,7 @@ class PCA(db.Model):
     
 with app.app_context():
     db.create_all()
+
+
+
+    
