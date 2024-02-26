78ce44c908b38d1e4888f6f9e7f5611d8717b786
farzhmz <112782889+farzhmz@users.noreply.github.com>
2024-02-26 12:38:38 +0000

    Your commit message

--- Flask_Project/app.py
+++ Flask_Project/app.py
@@ -1,22 +1,62 @@
-from flask import Flask                 # also import render template, request..
+from flask import Flask, render_template, request, redirect, url_for
+import os
+import sqlite3
+import pandas as pd
 from flask_sqlalchemy import SQLAlchemy
-from Database.db_schema import db, app
-#import sql queries and other funtions from relevant directories
+from src.analysis.flask_admixture import main_population_code, plot_admixture_for_superpopulation, check_input, get_population_code, get_superpopulation_code
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
+db_path = "Software-Development-Project/instance/ArchGenome.db"
+app = Flask(__name__)
 
-# Define routes for backend here: 
+# Define routes
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("index.html")
 
-#homepage route
+@app.route('/admixture', methods=['GET'])
+def admixture():
+    # This route should simply show the form.
+    return render_template('admixture.html')
 
-#@app.route("/")
-#@app.route("/home")
-#def home():
-    #return render_template("index.html")
+@app.route('/process_admixture', methods=['POST'])
+def process_admixture():
+    try:
+        population_type = request.form['population_type']
+        selected_items = request.form.getlist('selected_population')  # Correct variable name
+        plot_filenames = []
 
-#admixture routes 
+        if population_type == 'superpopulation':
+            for full_name in selected_items:  # Use 'selected_items' here
+                superpopulation_code = get_superpopulation_code(full_name)
+                if superpopulation_code:  # This 'if' block should be inside the 'for' loop
+                    plot_files = plot_admixture_for_superpopulation(superpopulation_code)
+                    plot_filenames.extend(plot_files)
+                else:
+                    print(f"Superpopulation code for '{full_name}' not found.")
+        elif population_type == 'population':
+            for full_name in selected_items:  # Use 'selected_items' here as well
+                population_code = get_population_code(full_name)
+                if population_code:
+                    plot_files = main_population_code(population_code)
+                    plot_filenames.extend(plot_files)
+                else:
+                    print(f"Population code for '{full_name}' not found.")
 
-#@app.route("")
+        return render_template('admixtureresults.html', plot_files=plot_filenames)
+    except Exception as e:
+        error_message = f"An error occurred: {e}"
+        app.logger.error(error_message)
+        return error_message, 500
 
+        
+
+# Run the application
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
 #population analysis routes
